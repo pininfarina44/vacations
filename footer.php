0d53abd6d4638474cc365fd470b465f388c00fee
berkay <berkaysurmeli@hotmail.com>
2018-10-06 09:01:42 -0700

    initial commit

---
+++ footer.php
@@ -0,0 +1,5 @@
+	<div class="footer">
+		<p>Ukay vacations app <?php echo date('Y'); ?></p>
+	</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Ukay Vacations</title>
+	<script type="text/javascript" src="js/main.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Ukay Vacation Days</h2>
+		<nav class="mainNav">
+			<ul>
+				<li><a href="vacations.php">Employee Vacations</a></li>
+				<li><a href="employees.php">Employees</a></li>
+			</ul>
+		</nav><!--MainNav-->
+	</div><!--header-->
