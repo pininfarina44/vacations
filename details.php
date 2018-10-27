70370d1c92668bdf75201eaa0b848c3845c51e71
berkay surmeli <berkaysurmeli@hotmail.com>
2018-10-26 19:28:11 -0700

    details page updated

--- details.php
+++ details.php
@@ -1,6 +1,7 @@
 	<?php include("header.php"); ?>
 
 
+
 		<div class="vacation-table-container">
 		<h2><?php echo $_POST["empName"] . " " . $_POST["empLastName"];?></h2>
 		<table class="vacation-table">
@@ -8,11 +9,22 @@
 				<tr class="vacation-table-header">
 					<th>Start date</th>
 					<th>End date</th>
+					<th>Days</th>
 					<th>Type</th>
 				</tr>
 			</thead>
 			<tbody>
 				<?php
+
+					function prepareDate($dateString){
+						$datePieces = explode("-", $dateString);
+						$year = $datePieces[0];
+						$month = $datePieces[1];
+						$day = $datePieces[2];
+
+					}
+					
+
 					//database variables
 					$serverName = "localhost";
 					$userName = "root";
@@ -38,20 +50,10 @@
 							echo "<tr class=\"vacation-row\">";
 							echo "<td>" . $row["startdate"] . "</td>";
 							echo "<td>" . $row["enddate"] . "</td>";
-							echo "<td>" . $row["type"] . "</td></tr>";
-							function myGetType($row["startdate"])
-    {
-        if (is_array($var)) return "array";
-        if (is_bool($var)) return "boolean";
-        if (is_float($var)) return "float";
-        if (is_int($var)) return "integer";
-        if (is_null($var)) return "NULL";
-        if (is_numeric($var)) return "numeric";
-        if (is_object($var)) return "object";
-        if (is_resource($var)) return "resource";
-        if (is_string($var)) return "string";
-        return "unknown type";
-    }
+							
+
+							echo "<td>" . $daysUsed . "</td>";
+							echo "<td>" . $row["type"] . "</td></tr>";	
 						}
 					}
 					else{
